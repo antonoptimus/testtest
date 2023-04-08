e1d076a7db1cdf5948500463a691720fe19764d4
Anton <anton_saintp@mail.ru>
2023-04-08 13:09:35 +0300

    2 commit

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const numberOfFilms = promt('Сколько фильмов вы уже посмотрели?', '')
+const numberOfFilms = +promt('Сколько фильмов вы уже посмотрели?', '')
 
 personalMovieDB = {
    count: numberOfFilms,
-   movies: '',
-   actors: '',
-   genres: '',
+   movies: {},
+   actors: {},
+   genres: [],
    privat: false
 }
 
-const film = promt('Один из последних просмотренных фильмов?', '')
-const rate = promt('На сколько оцените его?', '')
+const film = promt('Один из последних просмотренных фильмов?', ''),
+      rate = +promt('На сколько оцените его?', ''),
+      film2 = promt('Один из последних просмотренных фильмов?', ''),
+      rate2 = +promt('На сколько оцените его?', '');
 
-const movies = {
-   film: rate
-}
+personalMovieDB.movies[film] = rate;
+personalMovieDB.movies[film2] = rate2;
+
+console.log(personalMovieDB)
 
